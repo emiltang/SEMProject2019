f56191e6ec3421200057bd271240ca59d3682479
anders-i <aning15@student.sdu.dk>
2019-12-04 13:46:57 +0100

    CreateWarningFragment Spinner added
    Added spinner with application list to create warning

--- app/src/main/java/com/example/privacyapp/model/PrivacyWarning.kt
+++ app/src/main/java/com/example/privacyapp/model/PrivacyWarning.kt
@@ -2,11 +2,12 @@ package com.example.privacyapp.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
 @Entity(tableName = "warning")
 data class PrivacyWarning(
-    val id: UUID = UUID.randomUUID(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "app") val app: String,
     @ColumnInfo(name = "permission") val permission: String,
     @ColumnInfo(name = "description") val description: String
--- app/src/main/java/com/example/privacyapp/ui/fragment/CreateWarningFragment.kt
+++ app/src/main/java/com/example/privacyapp/ui/fragment/CreateWarningFragment.kt
@@ -1,7 +1,10 @@
 package com.example.privacyapp.ui.fragment
 
+import android.content.pm.ApplicationInfo
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
@@ -10,20 +13,25 @@ import com.example.privacyapp.R
 import com.example.privacyapp.service.UploadWorker
 import kotlinx.android.synthetic.main.fragment_create_warning.*
 
-class CreateWarningFragment : Fragment(R.layout.fragment_create_warning), View.OnClickListener {
+class CreateWarningFragment : Fragment(R.layout.fragment_create_warning), AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     lateinit var workManager: WorkManager
 
+    private val list = mutableListOf<String>()
+    private var apps = emptyList<ApplicationInfo>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        appTitleSpinner.onItemSelectedListener = this
         sendButton.setOnClickListener(this)
         workManager = WorkManager.getInstance(context!!)
+        loadApplicationInfoToSpinner()
     }
 
     private fun handleSendButton(view: View) {
 
         val data = Data.Builder()
-            .putString("app", appTitle.text.toString())
+            .putString("app", appTitleSpinner.selectedItem.toString())
             .putString("permission", permission.text.toString())
             .putString("description", description.text.toString())
             .build()
@@ -35,5 +43,23 @@ class CreateWarningFragment : Fragment(R.layout.fragment_create_warning), View.O
         when (view.id) {
             R.id.sendButton -> handleSendButton(view)
         }
+    }
+
+    private fun loadApplicationInfoToSpinner () {
+        apps = context!!.packageManager.getInstalledApplications(0)
+        apps.forEach{
+            list.add(context!!.packageManager.getApplicationLabel(it).toString())
+        }
+        val adapter = ArrayAdapter(context!!, android.R.layout.simple_list_item_1, list)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        appTitleSpinner.adapter = adapter
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        println("The selected item is: " + list[position] + " is it the same " + appTitleSpinner.selectedItem.toString())
     }
 }
