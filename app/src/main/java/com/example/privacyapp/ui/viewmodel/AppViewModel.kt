04e7988cad77287627f745b915cbf5c756296125
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2019-12-09 12:33:20 +0100

    Wrap unchecked cast in try catch

--- app/src/main/java/com/example/privacyapp/ui/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/example/privacyapp/ui/viewmodel/AppViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.*
 import com.example.privacyapp.db.AppDatabase
 import com.example.privacyapp.model.NetworkActivityRecord
@@ -16,8 +17,9 @@ class AppViewModel(
     private val applicationInfo: ApplicationInfo
 ) : AndroidViewModel(application), AppResultReceiver.AppReceiver {
 
-    val netData: MutableLiveData<List<NetworkActivityRecord>> get() = _netData
+    private val tag = this::class.simpleName
 
+    val netData: MutableLiveData<List<NetworkActivityRecord>> get() = _netData
     val warnings: LiveData<List<PrivacyWarning>> = AppDatabase(this.getApplication()).privacyWarningDao().getAll()
 
     private val _netData: MutableLiveData<List<NetworkActivityRecord>> by lazy {
@@ -38,7 +40,11 @@ class AppViewModel(
     }
 
     override fun onReceiveResult(resultCode: Int, bundle: Bundle) {
-        _netData.value = bundle.get("data") as ArrayList<NetworkActivityRecord>
+        try {
+            _netData.value = bundle.get("data") as ArrayList<NetworkActivityRecord>
+        } catch (ex: ClassCastException) {
+            Log.e(tag, "Wrong bundle type for 'data'", ex)
+        }
     }
 
     class NetworkStatsViewModelFactory(
