661be2ec777b5d8c5cddc6da36dce89a0a5b5f8b
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2019-12-09 14:12:13 +0100

    Refactor NetworkUsageService.kt

--- app/src/main/java/com/example/privacyapp/service/NetworkUsageService.kt
+++ app/src/main/java/com/example/privacyapp/service/NetworkUsageService.kt
@@ -4,7 +4,8 @@ import android.app.usage.NetworkStats
 import android.app.usage.NetworkStatsManager
 import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
+import android.net.ConnectivityManager.TYPE_MOBILE
+import android.net.ConnectivityManager.TYPE_WIFI
 import android.os.RemoteException
 import android.os.ResultReceiver
 import android.telephony.TelephonyManager
@@ -13,17 +14,13 @@ import androidx.core.app.JobIntentService
 import androidx.core.os.bundleOf
 import com.example.privacyapp.model.NetworkActivityRecord
 import java.util.*
-import kotlin.collections.ArrayList
 
 class NetworkUsageService : JobIntentService() {
 
-    private lateinit var networkStatsManager: NetworkStatsManager
     private val tag = this::class.simpleName
 
-    override fun onCreate() {
-        super.onCreate()
-        networkStatsManager = applicationContext.getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager
-    }
+    private val telephonyManager by lazy { getSystemService(TELEPHONY_SERVICE) as TelephonyManager }
+    private val networkStatsManager by lazy { getSystemService(NETWORK_STATS_SERVICE) as NetworkStatsManager }
 
     override fun onHandleWork(intent: Intent) {
         val uid = intent.getIntExtra("uid", NO_UID)
@@ -36,67 +33,54 @@ class NetworkUsageService : JobIntentService() {
         resultReceiver.send(0, bundleOf("data" to wifi + mobile))
     }
 
-    private fun unpackBuckets(networkStats: NetworkStats): List<NetworkActivityRecord> {
-        val records = ArrayList<NetworkStats.Bucket>()
+    private fun unpackBuckets(networkStats: NetworkStats) = sequence {
         while (networkStats.hasNextBucket()) {
             val bucket = NetworkStats.Bucket()
             networkStats.getNextBucket(bucket)
-            records.add(bucket)
-        }
-        return records.map {
-            NetworkActivityRecord(
-                startTime = it.startTimeStamp,
-                endTime = it.endTimeStamp,
-                downBytes = it.rxBytes.toInt(),
-                upBytes = it.txBytes.toInt()
+            yield(
+                NetworkActivityRecord(
+                    startTime = bucket.startTimeStamp,
+                    endTime = bucket.endTimeStamp,
+                    downBytes = bucket.rxBytes.toInt(),
+                    upBytes = bucket.txBytes.toInt()
+                )
             )
         }
     }
 
-    private fun getNetworkStatsForUidWifi(uid: Int): List<NetworkActivityRecord> {
-        val networkStats: NetworkStats?
-        val tomorrow = Calendar.getInstance().also { it.add(Calendar.DATE, 1) }
-        try {
-            networkStats = networkStatsManager.queryDetailsForUid(
-                ConnectivityManager.TYPE_WIFI,
-                "",
-                0,
-                tomorrow.timeInMillis,
-                uid
-            )
-        } catch (ex: RemoteException) {
-            Log.e(tag, "Network error", ex)
-            return emptyList()
-        }
-        return unpackBuckets(networkStats = networkStats)
+    private fun getNetworkStatsForUidWifi(uid: Int) = try {
+        networkStatsManager.queryDetailsForUid(
+            TYPE_WIFI,
+            "",
+            0,
+            tomorrow(),
+            uid
+        ).use(::unpackBuckets).asIterable()
+    } catch (ex: RemoteException) {
+        Log.e(tag, "Network error", ex)
+        emptyList<NetworkActivityRecord>()
     }
 
-    private fun getNetworkStatsForUidMobile(uid: Int): List<NetworkActivityRecord> {
-        /*
-         * Broken in android 10
-         * https://stackoverflow.com/questions/55173823/i-am-getting-imei-null-in-android-q
-         */
-        val networkStats: NetworkStats?
-        val tomorrow = Calendar.getInstance().also { it.add(Calendar.DATE, 1) }
-
-        try {
-            val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-            networkStats = networkStatsManager.queryDetailsForUid(
-                ConnectivityManager.TYPE_MOBILE,
-                telephonyManager.subscriberId,
-                0,
-                tomorrow.timeInMillis,
-                uid
-            )
-        } catch (ex: RemoteException) {
-            Log.e(tag, "Network error", ex)
-            return emptyList()
-        } catch (ex: SecurityException) {
-            Log.e(tag, "Missing permission", ex)
-            return emptyList()
-        }
-        return unpackBuckets(networkStats = networkStats)
+    /**
+     * Broken in android 10
+     * https://stackoverflow.com/questions/55173823/i-am-getting-imei-null-in-android-q
+     */
+    private fun getNetworkStatsForUidMobile(uid: Int) = try {
+        networkStatsManager.queryDetailsForUid(
+            TYPE_MOBILE,
+            telephonyManager.subscriberId,
+            0,
+            tomorrow(),
+            uid
+        ).use(::unpackBuckets).asIterable()
+    } catch (ex: RemoteException) {
+        Log.e(tag, "Network error", ex)
+        emptyList<NetworkActivityRecord>()
+    } catch (ex: SecurityException) {
+        Log.e(tag, "Missing permission", ex)
+        emptyList<NetworkActivityRecord>()
     }
+
 
     companion object {
         private const val NO_UID = -10
@@ -105,5 +89,7 @@ class NetworkUsageService : JobIntentService() {
         fun enqueueWork(context: Context, intent: Intent) = enqueueWork(
             context, NetworkUsageService::class.java, JOB_ID, intent
         )
+
+        private fun tomorrow() = Calendar.getInstance().also { it.add(Calendar.DATE, 1) }.timeInMillis
     }
 }
